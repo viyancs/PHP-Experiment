addbc5f5534236b10840772920e4ec65e9f73362
viyancs <msofyancs@gmail.com>
2012-11-12 10:09:49 +0700

    adding money return php

---
+++ money-return/money-return.php
@@ -0,0 +1,65 @@
+<?php
+
+function money_return($price,$pay) {
+    $info = 'success';
+    $money = array();
+    $money[0] = 50;
+    $money[1] = 100;
+    $money[2] = 200;
+    $money[3] = 500;
+    $money[4] = 1000;
+    $money[5] = 2000;
+    $money[6] = 5000;
+    $money[7] = 10000;
+    $money[8] = 20000;
+    $money[9] = 50000;
+    $money[10] = 100000;
+    $returnMoney = array();
+    $counter = 0;
+
+   //checking if type is not number 
+   if(is_numeric($price) === false || is_numeric($pay) === false){ return 'invalid input,input must be as number';}
+   
+   //checking if pay same with price
+   if($pay === $price) {
+      $returnMoney[$pay] = ++$counter;
+      $counter = 0;
+      return $returnMoney;
+   }
+
+   //checking if price more big than pay, is not relevan data
+   else if($price > $pay) return 'your money is not enough';
+
+   //price is possible < pay ,is relevan data, so process now
+   $fixReturn = $pay - $price;
+   //reverse array for counting from bigger number to lower, this case for otimal result
+   $money = array_reverse($money);
+   
+   for($i=0;$i<count($money);$i++) {
+      
+      //filter devide not more big than fixReturn
+      if($money[$i] <= $fixReturn) {
+
+         $opportunities =  $fixReturn / $money[$i];
+
+         if($opportunities % 1 === 0) {
+            $fixReturn = $fixReturn - $money[$i] * $opportunities;
+            $returnMoney[$money[$i]] = $opportunities;
+         }
+      
+         else {
+             
+            $fixReturn = $fixReturn - $money[$i] * floor($opportunities);
+            $returnMoney[$money[$i]] = floor($opportunities);
+         }
+      }
+
+      if($fixReturn < $money[10] && $fixReturn != 0) $info = "we don't have money for pay " . $fixReturn;
+
+   }
+   return $returnMoney;
+}
+
+$rm = money_return(1000,100000);
+var_dump($rm);
+?>
