1fd5dfdec81560f625cf19375b76cc20032ee812
viyancs <msofyancs@gmail.com>
2012-11-27 09:28:25 +0700

    update by simple code

--- money-return/money-return.php
+++ money-return/money-return.php
@@ -1,6 +1,6 @@
 <?php
 
-function money_return($price,$pay) {
+function money_return($price, $pay) {
     $info = 'success';
     $money = array();
     $money[0] = 50;
@@ -17,49 +17,55 @@ function money_return($price,$pay) {
     $returnMoney = array();
     $counter = 0;
 
-   //checking if type is not number 
-   if(is_numeric($price) === false || is_numeric($pay) === false){ return 'invalid input,input must be as number';}
-   
-   //checking if pay same with price
-   if($pay === $price) {
-      $returnMoney[$pay] = ++$counter;
-      $counter = 0;
-      return $returnMoney;
-   }
+    //checking if type is not number 
+    if (is_numeric($price) === false || is_numeric($pay) === false) {
+        return 'invalid input,input must be as number';
+    }
 
-   //checking if price more big than pay, is not relevan data
-   else if($price > $pay) return 'your money is not enough';
+    //checking if pay same with price
+    if ($pay === $price) {
+        $returnMoney[$pay] = ++$counter;
+        $counter = 0;
+        return $returnMoney;
+    }
 
-   //price is possible < pay ,is relevan data, so process now
-   $fixReturn = $pay - $price;
-   //reverse array for counting from bigger number to lower, this case for otimal result
-   $money = array_reverse($money);
-   
-   for($i=0;$i<count($money);$i++) {
-      
-      //filter devide not more big than fixReturn
-      if($money[$i] <= $fixReturn) {
+    //checking if price more big than pay, is not relevan data
+    else if ($price > $pay)
+        return 'your money is not enough';
 
-         $opportunities =  $fixReturn / $money[$i];
+    //price is possible < pay ,is relevan data, so process now
+    $fixReturn = $pay - $price;
 
-         if(!is_float($opportunities) ) {
+    //reverse array for counting from bigger number to lower, this case for otimal result
+    $money = array_reverse($money);
+
+    for ($i = 0; $i < count($money); $i++) {
+
+        //filter devide not more big than fixReturn
+        if ($money[$i] > $fixReturn) {
+            continue;
+        }
+        
+        //devide with optimum opportunities
+        $opportunities = $fixReturn / $money[$i];
+        
+        //make sure $opportunities not decimal
+        if (!is_float($opportunities)) {
             $fixReturn = $fixReturn - $money[$i] * $opportunities;
             $returnMoney[$money[$i]] = $opportunities;
-         }
-      
-         else {
-             
+        } else {
+
             $fixReturn = $fixReturn - $money[$i] * floor($opportunities);
             $returnMoney[$money[$i]] = floor($opportunities);
-         }
-      }
+        }
 
-      if($fixReturn < $money[10] && $fixReturn != 0) $info = "we don't have money for pay " . $fixReturn;
-
-   }
-   return $returnMoney;
+        //if not have money to pay return fix
+        if ($fixReturn < $money[10] && $fixReturn != 0)
+            $info = "we don't have money for pay " . $fixReturn;
+    }
+    return $returnMoney;
 }
 
-$rm = money_return(1000,100000);
+$rm = money_return(1000, 100000);
 var_dump($rm);
 ?>
