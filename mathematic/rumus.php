f0d1f8e699fc624216c5a6e3b10bd295df801b06
nugraha <mangcoding@gmail.com>
2015-05-07 19:42:14 +0700

    ganti rumus algorithm

--- mathematic/rumus.php
+++ mathematic/rumus.php
@@ -1,16 +1,15 @@
 <?PHP
 /*
-mencari angka 14 dari perpangkatan 10
 mathamatic calculating
 14 of 100 = 1;
-14 of 1000 = 1(10)+10 = 20
-14 of 10000 = 2(100)+100 - 1 = 299 --> -1
-14 of 10 ^ 5 = 3(10^3)+10^3 = 3000+1000 = 3970 --> -30 100.000
-3970*10 + 10000 - 299 = 49700 - 299 = 49401
-14 of 10 ^ 6 = 4(10^4)+10^4 = 40000+10000 = 49401 --> -599 1.000.000
-14 of 10 ^ 7 = 5(10 ^5)+10 ^5 = 500000+100000 = 590040 --> -9960 10.000.000
-14 of 10 ^ n = [n-2](10^[n-2])+10^[n-2] - ???
+14 of 1000 = 1*10+10-0 = 20;
+14 of 10000 = 20*10+100 - 1 = 299;
+14 of 10 ^ 5 = 299*10+10^3 -20 = 3970;
+14 of 10 ^ 6 = 3970*10+10^4 - 299 = 49401;
+14 of 10 ^ 7 = 49401*10+10^5 - 3970 = 590040
+14 of 10 ^ n = hasil dari 
 */
+// Hanya Berfungsi pada perpangkatan 10 saja, untuk angka lain saya belom menemukan solusinya
 
 function rumus($angka){
 	if ($angka <14) return 0;
@@ -18,9 +17,27 @@ function rumus($angka){
 	else {
 		$digit = strlen($angka)-1;
 		$x = ($digit-2);
-		$isi = rumus(pow(10,$x+1))*10 - rumus(pow(10,$x));
+		$isi = rumus(pow(10,$x+1))*10 + pow(10,$x) - rumus(pow(10,$x));
 		return $isi;
 	}
 }
 
+echo rumus(10000);
+echo "<br />";
 echo rumus(100000);
+echo "<br />";
+echo rumus(1000000);
+echo "<br />";
+echo rumus(10000000); //10.000.000
+echo "<br />";
+echo rumus(100000000);
+echo "<br />";
+echo rumus(1000000000);
+echo "<br />";
+echo rumus(10000000000); //10.000.000.000
+echo "<br />";
+echo rumus(100000000000);
+echo "<br />";
+echo rumus(1000000000000); //1.000.000.000.000
+echo "<br />";
+?>
